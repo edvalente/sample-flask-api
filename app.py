e8465840b977eddaa99f6258878178b32bbf0936
Edgar <edvalente@gmail.com>
2019-04-07 12:16:48 -0300

    Moves full app to app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 import os
 
-from api.sample import app
+import json
+import utils
+from flask import Flask, Response, abort
+
+app = Flask(__name__)
+
+@app.route('/files')
+def item_list():
+
+    # Create this
+    response = Response(
+        json.dumps('fixme'), status=200, mimetype='application/json')
+    return response
+
+
+@app.route('/files/<int:pk>')
+def item_score(pk):
+
+    # Create this
+    score = utils.get_score(pk)
+
+    if score is None:
+        abort(404)
+
+    content = json.dumps(score)
+    return content, 200, {'Content-Type': 'application/json'}
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return '', 404
 
 if __name__ == '__main__':
-    app.debug = True
-    # app.config['DATABASE_NAME'] = 'library.db'
-    host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host=host, port=port)
+    app.run(debug=True,host='0.0.0.0')
